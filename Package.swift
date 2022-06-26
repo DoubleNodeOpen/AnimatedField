5d71cb2a05d9c8c35033155387b2cfb313fdbd3b
Darren Ehlers <me@darrenehlers.com>
2022-06-25 21:44:10 -0500

    Rework directories for SPM

--- Package.swift
+++ Package.swift
@@ -19,15 +19,12 @@ let package = Package(
     ],
     dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "AnimatedField",
-            dependencies: [],
-            path: "AnimatedField",
-            resources: [
-                .copy("AnimatedField.xib")
-            ]
-        ),
-    ]
+         .target(
+             name: "AnimatedField",
+             dependencies: [],
+             resources: [
+                 .copy("Classes/AnimatedField.xib")
+             ]
+         )
+     ]
 )
