5d3ce00fd116c3800bfce10db5dcc34a2b4f21aa
Darren Ehlers <me@darrenehlers.com>
2022-06-26 21:56:13 -0500

    Improved Bundle lookup method

--- Package.swift
+++ Package.swift
@@ -20,15 +20,14 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/DoubleNodeOpen/swift-mask-textfield.git", from: "1.1.2"),
+        .package(url: "https://github.com/DoubleNode/DNSCore.git", from: "1.8.0"),
     ],
     targets: [
          .target(
              name: "AnimatedField",
              dependencies: [
+                "DNSCore",
                 .product(name: "SwiftMaskTextfield", package: "swift-mask-textfield")
-             ],
-             resources: [
-                 .copy("Classes/AnimatedField.xib")
              ]
          )
      ]
