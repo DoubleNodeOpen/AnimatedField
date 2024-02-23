27181df2801e9f213e0605cfa3300d6b3dad841b
Darren Ehlers <me@darrenehlers.com>
2024-02-23 15:24:25 -0600

    Fixed issue with duplicate strings

--- Sources/AnimatedField/AnimatedField+TextViewDelegate.swift
+++ Sources/AnimatedField/AnimatedField+TextViewDelegate.swift
@@ -19,6 +19,11 @@ extension AnimatedField: UITextViewDelegate {
             return shouldChange
         }
         
+        var text = text
+        // Apply uppercased & lowercased if available
+        if uppercased { text = text.uppercased() }
+        if lowercased { text = text.lowercased() }
+        
         // Copy new character
         var newInput = text
 
@@ -31,10 +36,6 @@ extension AnimatedField: UITextViewDelegate {
             textView.text = textView.text?.replacingOccurrences(of: invalidCharacter, with: "")
         }
 
-        // Apply uppercased & lowercased if available
-        if uppercased { newInput = newInput.uppercased() }
-        if lowercased { newInput = newInput.lowercased() }
-        
         // Limits & Regular expressions
         let limit = dataSource?.animatedFieldLimit(self) ?? Int.max
         let typingExpression = type.typingExpression
@@ -66,8 +67,10 @@ extension AnimatedField: UITextViewDelegate {
                 }
             }
         }
-        
-        if newInput == "" { return true }
+        if newInput.isEmpty {
+            return true
+        }
+        textView.text = newInput
         
         // Check limits
         return textView.text?.count ?? 0 + newInput.count < limit
